93c87653f9a73b50d1ac3ac95db401d6fd39a19b
Roeich <roeichsheikh@gmail.com>
2025-03-03 01:38:41 +0600

    working on create voucher page

--- assets/js/create_voucher.js
+++ assets/js/create_voucher.js
@@ -15,4 +15,33 @@ $(document).ready(function(){
     }, function(start, end, label) {
         console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
     });
+
+    // image upload previewer 
+    $('.img_inpFile').on('change', function(event) {
+        let input = event.target;
+        let preview = $(this).siblings('.img_inpPreview');
+        let box = $(this).closest('.img_inpBox'); 
+        let defaultImage = preview.attr('src');
+
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function(e) {
+                preview.attr('src', e.target.result);
+                box.addClass('img_uploaded'); 
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+
+        preview.data('default-src', defaultImage);
+    });
+
+    $('.img_inpResetBtn').on('click', function() {
+        let box = $(this).closest('.img_inpBox');
+        let preview = $(this).siblings('.img_inpPreview');
+        let input = $(this).siblings('.img_inpFile');
+
+        preview.attr('src', preview.data('default-src'));
+        input.val('');
+        box.removeClass('img_uploaded'); // Remove class when reset
+    });
 })
